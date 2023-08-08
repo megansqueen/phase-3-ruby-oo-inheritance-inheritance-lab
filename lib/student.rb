8c9a72d3ebb407bcb1b7b67a775aad7dbdc47ffa
Megan <esler6724@gmail.com>
2023-08-07 19:18:17 -0700

    assignment complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        return @knowledge
+    end
 
 end
